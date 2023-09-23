0cc233dc54a43344d6c1af2c64450b2b0c246f24
vivek kumar <vickythebest008@gmail.com>
2023-09-22 17:14:14 -0700

    leetcode-125

--- leetcode-easy/leetcode-125/validpalindrome.go
+++ leetcode-easy/leetcode-125/validpalindrome.go
@@ -1,14 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 func main() {
-
-	fmt.Println(isPalindrome("ram"))
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
 }
 
 func isPalindrome(s string) bool {
 
-	return true
+	regex := regexp.MustCompile("[^a-zA-Z0-9]+")
+	str := regex.ReplaceAllString(s, "")
 
+	str1 := strings.ToLower(str)
+
+	left := 0
+	right := len(str1) - 1
+	for left < right {
+		// fmt.Println(" str1[lenght] : ", str1[lenght])
+		if string(str1[left]) != string(str1[right]) {
+			return false
+		}
+		left++
+		right--
+	}
+
+	// if len(str1)%2 == 0 {
+	// 	fmt.Println("Even lenght of string : ", str1)
+
+	// } else {
+	// 	fmt.Println("Odd lenght of string : ", str1)
+	// }
+	return true
 }
