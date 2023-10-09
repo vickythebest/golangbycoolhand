f4d87f9a7c98ee7c7136236e1f7baf8aa29cbde4
vivek kumar <vickythebest008@gmail.com>
2023-10-09 09:48:51 -0700

    added medium and hard folders

---
+++ leetcode-hard/leetcode1216.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+Given a string s and an integer k, find out if the given string is a K-Palindrome or not.
+A string is K-Palindrome if it can be transformed into a palindrome by removing at most k characters from it.
+Example 1:
+	Input: s = "abcdeca", k = 2
+	Output: true
+	Explanation: Remove 'b' and 'e' characters.
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Println("call ")
+	plaindrom("test")
+}
+
+func plaindrom(s string) {
+	fmt.Println("hello", s)
+}
